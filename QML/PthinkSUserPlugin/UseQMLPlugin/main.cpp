79dd647ae4461f0dd67ec3877e6a5ab029210342
QtDheeru <dheerendra@pthinks.com>
2019-01-01 21:35:45 +0530

    Adding Comments

--- QML/PthinkSUserPlugin/UseQMLPlugin/main.cpp
+++ QML/PthinkSUserPlugin/UseQMLPlugin/main.cpp
@@ -1,5 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+/*
+ * http://www.pthinks.com - dheerendra@pthinks.com
+ * This example shows how to use the QML plugin
+ */
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +12,9 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    //DESTDIR = $$PWD/../bins - Directory where plugin exist
+    //This comes from Plugin pro file
+    //So replace the followign path appropriately
     engine.addImportPath("/Users/dheeru/Desktop/TSTS/bins");
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
